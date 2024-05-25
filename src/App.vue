7db0728bcab266f789b706807a93e0828be93676
smirnov <smirnov@alfasatcom.ru>
2024-05-25 19:43:18 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
+<template>
+  <!-- <h1 class="text-3xl font-bold underline">Hello world!</h1> -->
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
+    <a href="#">
+      <img src="./assets/logo.svg" class="h-9" />
+      <!-- <div>Aloha is vue 3 here</div> -->
+    </a>
+    <a href="#" class="text-sm">
+      <div class="flex items-center gap-2">
+        Day is complete
+        <CheckCircleIcon class="h-7 text-green-500" />
+      </div>
+    </a>
+  </header>
+  <main>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci repellat distinctio
+    culpa esse voluptate velit reprehenderit ullam quod itaque expedita repellendus facere dolorum
+    numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+    Unde adipisci repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque
+    expedita repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor
+    sit, amet consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate
+    velit reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex. Lorem ipsum dolor sit, amet
+    consectetur adipisicing elit. Unde adipisci repellat distinctio culpa esse voluptate velit
+    reprehenderit ullam quod itaque expedita repellendus facere dolorum numquam, dolore modi
+    ducimus, enim ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde adipisci
+    repellat distinctio culpa esse voluptate velit reprehenderit ullam quod itaque expedita
+    repellendus facere dolorum numquam, dolore modi ducimus, enim ex.
+  </main>
+</template>
